72c831009b0d30d658dcb0d3bd17c4eef410c3cb
Chris Mulder <30597826+muldercw@users.noreply.github.com>
2024-10-11 13:05:17 -0400

    Update first_page.py

--- pages/first_page.py
+++ pages/first_page.py
@@ -150,7 +150,7 @@ def get_stream_url(video_url):
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
             'Accept-Language': 'en-US,en;q=0.9',
             'Referer': 'https://www.youtube.com/',
-            'api_key': api_key,
+            'key': api_key,
         },
         'nocheckcertificate': True,  # In case certificates create issues
         'verbose': True,  # Optional: For more detailed output
